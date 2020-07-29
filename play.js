d262c9ef9f9b84c54a7079c65c8ad1f1d0c6613f
Kutluhan Duman <kutluduman@gmail.com>
2020-07-29 17:10:57 +0000

    Created connection to the server and also added the function to receive message from the server

---
+++ play.js
@@ -0,0 +1,29 @@
+const net = require('net');
+
+/**
+ * Establishes connection with the game server
+ */
+
+ const connect = function() {
+
+  const conn = net.createConnection({
+    port : 50541 ,
+    host : '135.23.222.131'
+  });
+
+  // event handle for receiving incoming message from server
+  conn.on('data', function (message) {
+    console.log(message);
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding('utf8'); 
+
+  return conn;
+
+ };
+
+ 
+
+ console.log('Connecting ...');
+connect();
