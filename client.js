3b3b8ca355bad8ed4e7435830c21fe9f7374ff73
Kutluhan Duman <kutluduman@gmail.com>
2020-07-29 17:52:02 +0000

    Sending the name to the server

--- client.js
+++ client.js
@@ -11,10 +11,18 @@ const connect = function() {
     host : '135.23.222.131'
   });
 
+  // event hand for successfully connection
+  conn.on('connect', function () {
+    console.log('Successfully connected to game server');
+    // sending the name to the server as JOK
+    conn.write(" Name: JOK");
+  });
+
   // event handle for receiving incoming message from server
   conn.on('data', function(message) {
     console.log(message);
   });
+
 
   // interpret incoming data as text
   conn.setEncoding('utf8');
